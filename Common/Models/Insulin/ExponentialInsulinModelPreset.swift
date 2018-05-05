8f444c62af128c3534d83fb788be3607f3df8837
Alexandre Cossette <alex.cossette@gmail.com>
2018-05-05 16:29:44 -0400

    Revert "fiasp peak 60 => 55 minutes, duration 360 => 420 minutes"
    This reverts commit e9f9589ef7c425093c19153fd3096aecb0dc6ad0.

--- Common/Models/Insulin/ExponentialInsulinModelPreset.swift
+++ Common/Models/Insulin/ExponentialInsulinModelPreset.swift
@@ -24,7 +24,7 @@ extension ExponentialInsulinModelPreset {
         case .humalogNovologChild:
             return .minutes(360)
         case .fiasp:
-            return .minutes(420)
+            return .minutes(360)
         }
     }
 
@@ -35,7 +35,7 @@ extension ExponentialInsulinModelPreset {
         case .humalogNovologChild:
             return .minutes(65)
         case .fiasp:
-            return .minutes(55)
+            return .minutes(60)
         }
     }
 
